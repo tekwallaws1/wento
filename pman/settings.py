a43b791224f519f856c73128b8c453c2d9001d46
tekwalliot <tekwall.iot@gmail.com>
2022-04-20 16:54:00 +0530

    initial commit

--- pman/settings.py
+++ pman/settings.py
@@ -103,23 +103,23 @@ WSGI_APPLICATION = 'pman.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'db_sse',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Tekwall1@3',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     } 
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_sse',
+        'USER': 'tekwall',
+        'PASSWORD': 'Tekwall1@3',
+        'HOST': 'localhost',
+        'PORT': '',
+    } 
+}
 
 
 # Password validation
